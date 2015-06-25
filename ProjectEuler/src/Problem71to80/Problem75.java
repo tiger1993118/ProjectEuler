1d6390bb71c54b22e04b78f9775f0335f154b770
Tiger1993118 <yixiu17@gmail.com>
2015-06-24 22:08:55 -0400

    Added solution to Problem85

--- ProjectEuler/src/Problem71to80/Problem75.java
+++ ProjectEuler/src/Problem71to80/Problem75.java
@@ -7,14 +7,20 @@ package Problem71to80;
 
 import java.math.BigInteger;
 
+import Algorithm.Prime;
+
 /**
  * @author Xiu Tiger Yi
  *
  */
 public class Problem75 {
 
+	static int limit = 150_0000;
+
+	static int aLimit = 50_0000;
+
 	public static void singularIntegerRightTriangles() {
-		int limit = 1_500_000;
+
 		boolean[] isRightTri = new boolean[limit];
 		// BigInteger a_square = new BigInteger("0");
 		BigInteger square = BigInteger.valueOf(0);
@@ -58,9 +64,69 @@ public class Problem75 {
 		// }
 	}
 
+	// 105005874983
+	// 124905538120
+	public static void singularIntegerRightTriangles2() {
+		BigInteger square = BigInteger.valueOf(0);
+		BigInteger[] squares = new BigInteger[limit / 2 + 1];
+		for (Integer n = 1; n <= limit / 2; n++) {
+			square = square.add(BigInteger.valueOf(n));
+			square = square.add(BigInteger.valueOf(n - 1));
+			squares[n] = square;
+		}
 
+		Prime.setPrimes(aLimit + 10_0000);
+		boolean[] isPrime = Prime.getIsPrimes();
+
+		BigInteger aSquare = BigInteger.ZERO;
+		int estimate = 0;
+		BigInteger total = BigInteger.ZERO;
+		for (int a = 1; a < aLimit; a++) {
+			aSquare = squares[a];
+			if (!isPrime[a]) {
+				if (a < 1225) {
+					estimate = a * a / 2;
+				} else {
+					estimate = (limit - a) / 2;
+				}
+				for (int b = a + 1; b <= aLimit; b++) {
+					if (!isPrime[b]) {
+						int reminder = b % a;
+						if (reminder != 0 && b % reminder != 0) {
+							total = total.add(BigInteger.ONE);
+						}
+					}
+				}
+			}
+		}
+		System.out.println(total);
+	}
+
+	public static void singularIntegerRightTriangles3() {
+
+		int aSquare = 1, estimate, bSquare, cSquare;
+		double root;
+		for (int a = 2; a < aLimit; a++) {
+			aSquare += 2 * a - 1;
+			bSquare = aSquare;
+			cSquare = 2 * bSquare;
+			if (a < 1225) {
+				estimate = a * a / 2;
+			} else {
+				estimate = (limit - a) / 2;
+			}
+			for (int b = a + 1; b <= estimate; b++) {
+				cSquare += 2 * b - 1;
+				root = Math.sqrt(cSquare);
+				if ((int) root == root) {
+					System.out.println(a + "-" + b);
+				}
+			}
+		}
+	}
 
 	public static void main(String[] args) {
-		singularIntegerRightTriangles();
+		singularIntegerRightTriangles3();
+		//System.out.println((int) Math.sqrt(5));
 	}
 }
---
+++ ProjectEuler/src/Problem81to90/Problem85.java
@@ -0,0 +1,46 @@
+/**Problem85.java
+ * {todo}
+ @author Xiu Yi Tiger yixiu17@gmail.com
+ Created on Jun 24, 2015 at 9:31:25 PM
+ */
+package Problem81to90;
+
+/**
+ * @author Xiu Tiger Yi
+ *
+ */
+public class Problem85 {
+
+	public static void countingRectangles() {
+		int diff = 100000;
+		int goal = 200_0000;
+		int[] goal_area = { 0, 0 };
+		for (int w = 1; w < 2000; w++) {
+			int count = w * (w + 1) / 2;
+			int total = 0;
+			int l = 0;
+			while (total < goal) {
+				l++;
+				total += count * l;
+			}
+			if (total - goal < diff) {
+				diff = total - goal;
+				goal_area[0] = w;
+				goal_area[1] = l;
+				System.out.println(w + "-" + l + "-" + total);
+			}
+			if (goal - (total - count * l) < diff) {
+				diff = goal - (total - count * l);
+				goal_area[0] = w;
+				goal_area[1] = l;
+				System.out.println(w + "-" + (l - 1) + "-"
+						+ (total - count * l));
+			}
+
+		}
+	}
+
+	public static void main(String[] args) {
+		countingRectangles();
+	}
+}
