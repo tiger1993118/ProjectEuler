c7b12f59fae4d79382dbd8071d5dc3aaa0f1fa80
Tiger1993118 <yixiu17@gmail.com>
2015-07-05 00:50:30 -0400

    Added solution to Problem88

--- ProjectEuler/src/Problem81to90/Problem86.java
+++ ProjectEuler/src/Problem81to90/Problem86.java
@@ -11,9 +11,12 @@ package Problem81to90;
  */
 public class Problem86 {
 
+	public static Integer limit = 1819;
+
+	public static boolean[][] isInteger = new boolean[limit][limit * 2];
+
 	public static void cuboidRoute() {
 		Integer total = 0;
-		Integer limit = 2500;
 		for (Integer m = 2; m < limit; m++) {
 			// m:1 in 3
 			for (Integer b = 1; b < m; b++) {
@@ -30,9 +33,11 @@ public class Problem86 {
 				break;
 			}
 		}
+		System.out.println(total);
 
 	}
 
+	// 1818-1000457
 	public static boolean isCount(Integer a, Integer b, Integer m) {
 		double half = (double) (a + b + m) / 2;
 		double abs_a = Math.abs(a - half);
@@ -40,11 +45,9 @@ public class Problem86 {
 		double abs_m = Math.abs(m - half);
 		Integer side1, side2;
 		if (abs_a <= abs_b && abs_a <= abs_m) {// a
-			side1 = a;
-			side2 = b + m;
+			return isInteger[a][b + m];
 		} else if (abs_b <= abs_a && abs_b <= abs_m) {// b
-			side1 = b;
-			side2 = a + m;
+			return isInteger[b][a + m];
 		} else {// m
 			side1 = m;
 			side2 = a + b;
@@ -53,6 +56,7 @@ public class Problem86 {
 		double root = Math.round(Math.sqrt(hypo));
 		if (root * root == hypo) {
 			// System.out.println(a + "-" + b + "-" + m);
+			isInteger[m][a + b] = true;
 			return true;
 		}
 		return false;
---
+++ ProjectEuler/src/Problem81to90/Problem88.java
@@ -0,0 +1,91 @@
+/**Problem88.java
+ * {todo}
+ @author Xiu Yi Tiger yixiu17@gmail.com
+ Created on Jul 4, 2015 at 11:50:19 PM
+ */
+package Problem81to90;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import Algorithm.Prime;
+
+/**
+ * @author Xiu Tiger Yi
+ *
+ */
+public class Problem88 {
+
+	public static void product_sum_numbers() {
+		Integer size = 13000;
+		Integer limit = 12001;
+		Prime.setPrimes(size);
+		boolean[] isPrime = Prime.getIsPrimes();
+		BigInteger total = BigInteger.ZERO;
+		int[] minimals = new int[size];
+		List<Set<Integer>> nToKs = new ArrayList<Set<Integer>>();
+
+		for (Integer n = 0; n < size; n++) {
+			Set<Integer> ks = new HashSet<Integer>();
+			ks.add(1);
+			if (!isPrime[n]) {
+				for (Integer a = 2; a <= Math.sqrt(n); a++) {
+					if (n % a == 0) {
+						Integer b = n / a;
+						if (isPrime[a] && isPrime[b]) {
+							int k = n - a - b + 2;
+							minimals[k] = (minimals[k] == 0) ? n : Math.min(n,
+									minimals[k]);
+							ks.add(k);
+						} else if (isPrime[a]) {
+							for (Integer bK : nToKs.get(b)) {
+								int k = n - a - b + 2;
+								k = k + bK - 1;
+								minimals[k] = (minimals[k] == 0) ? n : Math
+										.min(n, minimals[k]);
+								ks.add(k);
+							}
+
+						} else if (isPrime[b]) {
+							for (Integer aK : nToKs.get(a)) {
+								int k = n - a - b + 2;
+								k = k + aK - 1;
+								minimals[k] = (minimals[k] == 0) ? n : Math
+										.min(n, minimals[k]);
+								ks.add(k);
+							}
+
+						} else {
+							for (Integer aK : nToKs.get(a)) {
+								for (Integer bK : nToKs.get(b)) {
+									int k = n - a - b + 2;
+									k = k + aK + bK - 2;
+									minimals[k] = (minimals[k] == 0) ? n : Math
+											.min(n, minimals[k]);
+									ks.add(k);
+								}
+							}
+						}
+					}
+				}
+			}
+			nToKs.add(ks);
+		}
+		Set<Integer> minimal = new HashSet<Integer>();
+		for (int k = 0; k < limit; k++) {
+			minimal.add(minimals[k]);
+		}
+		for (int m : minimal) {
+			total = total.add(BigInteger.valueOf(m));
+		}
+		System.out.println(total);
+	}
+
+	public static void main(String[] args) {
+		product_sum_numbers();
+	}
+
+}
