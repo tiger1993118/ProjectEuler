19a67d24b96f24dfb78bbfd64f7f93153cb4f190
YiXiu <yixiu17@gmail.com>
2018-08-15 23:23:53 -0400

    Add timing part in Problem 108
    Make use of timer, and time the solving time of problem 108

--- ProjectEuler/src/part2/problem101to110/Problem108.java
+++ ProjectEuler/src/part2/problem101to110/Problem108.java
@@ -7,6 +7,8 @@ package part2.problem101to110;
 
 import java.math.BigInteger;
 
+import algorithm.Timer;
+
 /**
  * @author Xiu Tiger Yi
  * TODO
@@ -21,10 +23,13 @@ import java.math.BigInteger;
 public class Problem108 {
 
 	public static void solve() {
+		
 		// From the algorithm, repsectively x and n
 		// Algorithm is calculating the y
 		BigInteger x, n = BigInteger.valueOf(50000), limit;
 		int count = 0;// Count the number of solutions
+		int printed = 100;
+		
 		while(count <= 1000) {
 			count = 0;// Reset the count
 			n = n.add(BigInteger.ONE); // try the next n
@@ -33,11 +38,16 @@ public class Problem108 {
 				if((n.multiply(x)).mod(x.subtract(n)).equals(BigInteger.ZERO)) 
 						count ++;
 			}
-			
+			if(count > printed) {
+				double time = Timer.duartion();
+				System.out.println("n is " + n + " and count is " + count + ", and it took " + time + " seconds");
+				printed += 100;
+			}
 		}
 		System.out.println("n is " + n + " and count is " + count);
 	}
 	public static void main(String[] args) {
+		Timer.start();
 		Problem108.solve();
 	}
 
