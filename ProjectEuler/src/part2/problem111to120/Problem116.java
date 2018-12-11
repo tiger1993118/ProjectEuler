4551c11332a546a869c83ddb79f4913c29b5d47d
Tigger GL12CP(WINDOWS) <yixiu17@gmail.com>
2018-12-10 19:30:30 -0500

    Add comment in Problem 116

--- ProjectEuler/src/part2/problem111to120/Problem116.java
+++ ProjectEuler/src/part2/problem111to120/Problem116.java
@@ -6,32 +6,32 @@ package part2.problem111to120;
  */
 public class Problem116 {
 
-	// Measuring the number of ways to replace 50 units of black tiles
+	// Measuring the number of ways to replace "LENGTH" units of black tiles
 	private static final int LENGTH = 51;
 
-	// 3 different tiles
+	// 3 different tiles: RED, GREEN and BLUE
 	private static final int TILES = 3;
 
-	// different lengths for different color of tiles
+	// lengths OF different color of tiles
 	private static final int RED = 2;
 	private static final int GREEN = 3;
 	private static final int BLUE = 4;
-	// for the convenience of looping
+	// for the convenience of looping and coding
 	private static final int[] COLORS = new int[] { RED, GREEN, BLUE };
 
 	/*
-	 * Main Idea: Solving the question using Dynamic Programming, and we will work
-	 * our solution bottom-up and save every number of ways in an array 'ways', for
-	 * which red_ways[i] represents the number of ways to replace tiles of length i
-	 * in red tiles(i.e. length two), so does green_ways[i] and blue_ways[i].
-	 * Lastly, add them up to compute the the total ways of replacement. ----------
-	 * NOTES: I assign red _ways[i] to represent the number of ways to cover the
-	 * black tiles that ALLOWING no red tile to be used(i.e. Solution could be all
-	 * Blacks), since this would make the dynamic programming much easier, and in
-	 * the end, just deduct 1 from the final solution.
+	 * Main Idea: I will solve this question using Dynamic Programming, and I will
+	 * work our solution bottom-up and save every number of ways in an array 'ways',
+	 * for which ways[i][n] represents the number of ways to replace tiles of length
+	 * 'n' with 'ith' tile (i.e. 0 represent RED, 1 - GREEN and 2 - BLUE). Lastly,
+	 * add them up to compute the the total ways of replacement. ---------- NOTES: I
+	 * assign _ways[i][n] to represent the number of ways to cover the black tiles
+	 * that ALLOWING all black tile (i.e. no tiles will be replaced), since this
+	 * would make the dynamic programming much easier. In the end, just deduct 1
+	 * from each color of solution.
 	 * 
-	 * 1. Initialize all three arrays representing the solution of three colors.--
-	 * 2. Different base case for three arrays.-----------------------------------
+	 * 1. Initialize all the 2D array ---------------------------------------------
+	 * 2. Different base case for different color.--------------------------------
 	 * 3. Iteratively run the bottom-up solutions. -------------------------------
 	 */
 	public static void solve() {
