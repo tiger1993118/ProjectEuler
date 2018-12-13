29dfc7eb877d1f9918520e03da36f688afad4a09
Tigger GL12CP(WINDOWS) <yixiu17@gmail.com>
2018-12-13 16:24:27 -0500

    Add comment in Problem 120

--- ProjectEuler/src/part2/problem111to120/Problem120.java
+++ ProjectEuler/src/part2/problem111to120/Problem120.java
@@ -7,6 +7,21 @@ public class Problem120 {
 
 	/*
 	 * Progress: Try expanding the equation first see to find clue
+	 * 
+	 * Main Idea: First keeping the equation E = (a-1)^n + (a+1)^n, and after
+	 * simplifying the equation, we have two result depending if n is odd or even.
+	 * 
+	 * if n is even, E = 2; if n is odd, E = 2*n*a.
+	 * 
+	 * We could ignore the case n is even, since the value(i.e. 2 mod a^2) is
+	 * smaller than n is odd (i.e. 2*n*a mod a^2)
+	 * 
+	 * And in the case of a, we could have a is even or odd
+	 * 
+	 * If a is odd, we could have the max value (a-1)*a (i.e. mod a^2) ;-----------
+	 * If a is even, we could the max (a-2)*a (i.e. mod a^2)
+	 *
+	 * Lastly add them up.
 	 */
 	public static void solve() {
 
@@ -14,9 +29,9 @@ public class Problem120 {
 
 		for (int a = 3; a <= 1000; a++) {
 
-			if (a % 2 == 0) {
+			if (a % 2 == 0) {// a is even
 				total += a * (a - 2);
-			} else {
+			} else {// a is odd
 				total += a * (a - 1);
 			}
 		}
