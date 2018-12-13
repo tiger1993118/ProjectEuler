70c18b0fc237a14e9e3cdc5b94b17fa371d1dde8
Tigger GL12CP(WINDOWS) <yixiu17@gmail.com>
2018-12-13 16:11:53 -0500

    Finish Problem 120

--- ProjectEuler/src/part2/problem111to120/Problem120.java
+++ ProjectEuler/src/part2/problem111to120/Problem120.java
@@ -1,9 +1,30 @@
 package part2.problem111to120;
 
+/**
+ * Problem 120: https://projecteuler.net/problem=120
+ */
 public class Problem120 {
 
+	/*
+	 * Progress: Try expanding the equation first see to find clue
+	 */
+	public static void solve() {
+
+		long total = 0;
+
+		for (int a = 3; a <= 1000; a++) {
+
+			if (a % 2 == 0) {
+				total += a * (a - 2);
+			} else {
+				total += a * (a - 1);
+			}
+		}
+		System.out.println(total);
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Problem120.solve();
 
 	}
 
